e071cd43ec3527be2be3477dad6531d41cb45679
Peter <peter.helle.hartmann@gmail.com>
2022-11-05 17:15:52 +0100

    saving this mess

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <div className='App'>
+      <aside>hi</aside>
+      <Deck />
+      <aside>buddy</aside>
+    </div>
+  );
+}
