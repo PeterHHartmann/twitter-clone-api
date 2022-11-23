61b906a8ee66b2f1bd1657753fd1946622eb9d51
Peter <peter.helle.hartmann@gmail.com>
2022-11-23 02:35:05 +0100

    profile get error handling and refactor

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,13 +1,19 @@
 import prisma from '../config/prisma';
 import { Router } from 'express';
+import { verifyAccess } from '../middleware/verifyAccess';
 const router = Router();
 
-router.get(`/:username`, async (req, res) => {
-  const result = await prisma.profile.findUnique({
-    where: { username: req.params.username },
-  });
-  if (!result) return res.status(404).json({});
-  return res.status(200).json(result);
+router.get(`/:username`, verifyAccess, async (req, res) => {
+  try {
+    const profile = await prisma.profile.findUnique({
+      where: { username: req.params.username },
+    });
+    if (!profile) return res.status(404).json({});
+    return res.status(200).json(profile);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({})
+  }
 });
 
 export default router;
